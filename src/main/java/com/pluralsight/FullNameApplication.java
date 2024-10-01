3d3add890642c87b288793011573fddfaa1d6a15
ProdJohn <fwtx.john@gmail.com>
2024-10-01 15:53:18 -0500

    more string practice

---
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,28 @@
+package com.pluralsight;
+
+import java.security.Key;
+import java.util.Scanner;
+
+public class FullNameApplication {
+    public static void main(String[] args) {
+        Scanner Keyboard = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String firstName = Keyboard.nextLine();
+        System.out.println("Now enter your middle name, if you don't have one press enter:");
+        String middleName = Keyboard.nextLine();
+        System.out.println("Enter your last name: ");
+        String lastName = Keyboard.nextLine();
+        System.out.println("If you have a suffix please enter it now, if not press enter: ");
+        String suffix = Keyboard.nextLine();
+        System.out.println("Your full name is: ");
+        System.out.println(firstName.trim() + " " + middleName.trim() + " " + lastName.trim() + " " + suffix);
+        Keyboard.close();
+
+
+
+
+
+
+
+    }
+}
